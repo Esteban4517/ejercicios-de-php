e0f47fd2995d9b498e6196c18f92e566c0cd6f83
Angel Esteban rivera Juarez <71298114+Esteban4517@users.noreply.github.com>
2020-12-06 14:29:54 -0600

    Create pi.php

---
+++ pi.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>estamos empezando a programar php...</h1>";
+for ($i=0; $i < ; $i++) { 
+	echo "valor de numero $i <br>";
+	# code...
+}
+
+for ($i=0; $i < ; $i++) { 
+	echi '<input type="text" name="txtc.$i."'>
+	$i . '"><br>';
+	# code..c
+}
+?>
